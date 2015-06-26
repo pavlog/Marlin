243733f89f542de9d95ac1e3437efbe4b0fe3682
pavlog <pavel.grib@gmail.com>
2015-06-26 16:29:00 +0300

    !U zmax = 125

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -118,7 +118,7 @@
 #define TEMP_SENSOR_0 1
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
-#define TEMP_SENSOR_BED -1
+#define TEMP_SENSOR_BED 0
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
 //#define TEMP_SENSOR_1_AS_REDUNDANT
@@ -383,7 +383,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define X_MIN_POS 0
 #define Y_MAX_POS 100
 #define Y_MIN_POS 0
-#define Z_MAX_POS 130
+#define Z_MAX_POS 125
 #define Z_MIN_POS MANUAL_Z_HOME_POS
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
