a5d125aa84fe5a78f79730f02aaaefddc6138f7b
pavlog <pavel.grib@gmail.com>
2015-06-26 16:47:09 +0300

    !U z max is 128

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -383,7 +383,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define X_MIN_POS 0
 #define Y_MAX_POS 100
 #define Y_MIN_POS 0
-#define Z_MAX_POS 125
+#define Z_MAX_POS 128
 #define Z_MIN_POS MANUAL_Z_HOME_POS
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
